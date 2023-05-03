4410308fe536aea8d1a25720ccf617a9b8b5c3e9
Anton Iakimov <yakimant@gmail.com>
2023-05-03 13:05:04 +0200

    A bit of refactoring

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # Requirements
-- Python and `python-consul2`
+- Python and packages, see `pyproject.toml`
 - `firewalld` installed and running
+
 # Configuration
 - `python-consul2` might require configuration
 - `firewall-cmd` might requre `root` access
+
 # Running
 - Run by `cron` with access to run `firewall-cmd` (`root` or any better way)
+
 # Debug / Testing
-- Can test without Consul with test file provided (see code)
-- Can test `firewall-cmd` commands without running (see code)
+- Can test without Consul with test file provided. See:
+  - `DEBUG_CONSUL_SERVICES_OVERRIDE_JSON_URL`
+  - `DEBUG_CONSUL_CURRENT_HOST_ENV_OVERRIDE`
+- Can test `firewall-cmd` commands without running (see `DEBUG_DRY_RUN`)
+
 # TODO
 - Some `firewall-cmd` will fail and should not be treated as failure
+- Error Handling in general
+- Documentation strings & comments if needed
+- Configuration and/or arguments for debug, logging
+- Tests
+- Find a firewall lib or extract `firewalld` methods to a module
+- Extract mapping rules from `add_services_to_rules` to configuration file
+
 # Better ways (need research)
 - [Consul Watches](https://developer.hashicorp.com/consul/docs/dynamic-app-config/watches): subscribe for Consul catalog updates
 - [Network Infrastructure Automation (NIA) with Terraform and Consul](https://www.hashicorp.com/resources/introduction-to-network-infrastructure-automation-nia-with-hashicorp-terraform-an)
--- firewall_updater.py
+++ firewall_updater.py
@@ -1,106 +1,214 @@
 import subprocess
 import consul
-# import requests
+import requests
+import logging
 
-custom_services = {
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+CUSTOM_SERVICES = {
     'node_exporter': ['9100/tcp'],
     'mysqld_exporter': ['9104/tcp'],
     'logstash_syslog': ['5141/udp', '5141/tcp']
 }
 
-def apply_firewall_rules(rules):
-    for zone, zone_rules in rules.items():
+DEBUG_DRY_RUN = True
+DEBUG_CONSUL_SERVICES_OVERRIDE_JSON_URL = 'https://gist.githubusercontent.com/jakubgs/dbf1df154f2d94541dc01baf1116d69f/raw/e2cccda0fba8988bc0af8a710ba5c5b4413d7558/services.json'
+DEBUG_CONSUL_CURRENT_HOST_ENV_OVERRIDE = 'app'
+
+c = consul.Consul()
+
+
+def create_firewall_zone(zone_name: str):
+    cmd = ['firewall-cmd',
+           '--permanent',
+           f'--new-zone={zone_name}']
+
+    if DEBUG_DRY_RUN:
+        logging.debug(cmd)
+    else:
+        subprocess.run(cmd, check=False)
+
+
+def create_new_service(service_name: str):
+    if service_name not in CUSTOM_SERVICES:
+        return
+
+    cmd = ['firewall-cmd',
+           '--permanent',
+           f'--new-service={service_name}']
+
+    if DEBUG_DRY_RUN:
+        logging.debug(cmd)
+    else:
+        subprocess.run(cmd, check=False)
+
+    add_ports_arguments = " ".join([
+        f'--add-port={a}' for a in CUSTOM_SERVICES[service_name]])
+
+    cmd = ['firewall-cmd',
+           '--permanent',
+           f'--service={service_name}',
+           add_ports_arguments]
+
+    if DEBUG_DRY_RUN:
+        logging.debug(cmd)
+    else:
+        subprocess.run(cmd, check=False)
+
+
+def add_service_to_zone(service_name: str, zone_name: str):
+    cmd = ['firewall-cmd',
+           '--permanent',
+           f'-zone={zone_name}',
+           f'--add-service={service_name}']
+
+    if DEBUG_DRY_RUN:
+        logging.debug(cmd)
+    else:
+        subprocess.run(cmd, check=False)
+
+
+def remove_source_from_zone(ipset_name: str, zone_name: str):
+    cmd = ['firewall-cmd',
+           '--permanent',
+           f'-zone={zone_name}',
+           f'--remove-source=ipset:{ipset_name}']
+
+    if DEBUG_DRY_RUN:
+        logging.debug(cmd)
+    else:
+        subprocess.run(cmd, check=False)
+
+
+def delete_ipset(ipset_name: str):
+    cmd = ['firewall-cmd',
+           '--permanent',
+           f'--delete-ipset={ipset_name}']
+
+    if DEBUG_DRY_RUN:
+        logging.debug(cmd)
+    else:
+        subprocess.run(cmd, check=False)
+
+
+def create_new_ipset(ipset_name: str, ips: str):
+    delete_ipset(ipset_name)
+
+    cmd = ['firewall-cmd',
+           '--permanent',
+           f'--new-ipset={ipset_name}',
+           '--type=hash:ip']
+    if DEBUG_DRY_RUN:
+        logging.debug(cmd)
+    else:
+        subprocess.run(cmd, check=False)
+
+    for ip in ips:
+        cmd = ['firewall-cmd',
+               '--permanent',
+               f'--ipset={ipset_name}',
+               f'--add-entry={ip}']
+        if DEBUG_DRY_RUN:
+            logging.debug(cmd)
+        else:
+            subprocess.run(cmd, check=False)
+
+
+def add_ipset_to_zone(ipset_name: str, zone_name: str):
+    cmd = ['firewall-cmd',
+           '--permanent',
+           f'--zone={zone_name}',
+           f'--add-source=ipset:{ipset_name}']
+    if DEBUG_DRY_RUN:
+        logging.debug(cmd)
+    else:
+        subprocess.run(cmd, check=False)
+
+
+def reload_firewall():
+    cmd = ['firewall-cmd',
+           '--reload']
+    if DEBUG_DRY_RUN:
+        logging.debug(cmd)
+    else:
+        subprocess.run(cmd, check=False)
+
+
+def apply_firewall_rules(rules: dict):
+    logging.info('Applying firewalld rules')
+    for zone_name, zone_rules in rules.items():
         if 'services' in zone_rules:
-            # print(f'firewall-cmd --permanent --new-zone={zone}')
-            subprocess.run(['firewall-cmd',
-                            '--permanent',
-                            f'--new-zone={zone}'], check=False)
+            create_firewall_zone(zone_name)
 
-            for service in zone_rules['services']:
-                # print(f'firewall-cmd --permanent --new-service={service}')
-                subprocess.run(['firewall-cmd',
-                                '--permanent',
-                                f'--new-service={service}'], check=False)
+            for service_name in zone_rules['services']:
+                create_new_service(service_name)
+                add_service_to_zone(service_name, zone_name)
 
-                add_ports_arguments = " ".join([
-                    f'--add-port={a}' for a in custom_services['logstash_syslog']])
+            ipset_name = zone_name
+            remove_source_from_zone(ipset_name, zone_name)
+            create_new_ipset(ipset_name, zone_rules['ips'])
+            add_ipset_to_zone(ipset_name, zone_name)
 
-                # print(f'firewall-cmd --permanent --service={service} {add_ports_arguments}')
-                subprocess.run(['firewall-cmd',
-                                '--permanent',
-                                f'--service={service}', add_ports_arguments], check=False)
+            reload_firewall()
 
-                # print(f'firewall-cmd --permanent --zone={zone} --add-service={service}')
-                subprocess.run(['firewall-cmd',
-                                '--permanent',
-                                f'-zone={zone}',
-                                f'--add-service={service}'], check=False)
-
-            # print(f'firewall-cmd --permanent --zone={zone} --remove-source=ipset:{zone}')
-            subprocess.run(['firewall-cmd',
-                            '--permanent',
-                            f'-zone={zone}',
-                            f'--remove-source=ipset:{zone}'], check=False)
-
-            # print(f'firewall-cmd --permanent --delete-ipset={zone}')
-            subprocess.run(['firewall-cmd',
-                            '--permanent',
-                            f'--delete-ipset={zone}'], check=False)
-
-            # print(f'firewall-cmd --permanent --new-ipset={zone} --type=hash:ip')
-            subprocess.run(['firewall-cmd',
-                            '--permanent',
-                            f'--new-ipset={zone}',
-                            '--type=hash:ip'], check=False)
-
-            for ip in zone_rules['ips']:
-                # print(f'firewall-cmd --permanent --ipset={zone} --add-entry={ip}')
-                subprocess.run(['firewall-cmd',
-                                '--permanent',
-                                f'--ipset={zone}',
-                                f'--add-entry={ip}'], check=False)
-
-            # print(f'firewall-cmd --permanent --zone={zone} --zone={zone}')
-            subprocess.run(['firewall-cmd',
-                            '--permanent',
-                            f'--zone={zone}',
-                            f'--zone={zone}'], check=False)
-
-    # print(f'firewall-cmd --reload')
-    subprocess.run(['firewall-cmd', '--reload'], check=False)
 
 def add_service_to_zone_rules(rules, zone, service):
+    logging.info(f'Adding rule to open {service} for {zone}')
     if 'services' in rules[zone]:
         rules[zone]['services'].append(service)
     else:
         rules[zone]['services'] = [service]
 
-c = consul.Consul()
-agent_info = c.agent.self()
-current_node_env = agent_info['Config']['NodeMeta']['env']
-# current_node_env = 'metrics'
-rules = {}
 
-# Test file
-# services = requests.get('https://gist.githubusercontent.com/jakubgs/dbf1df154f2d94541dc01baf1116d69f/raw/e2cccda0fba8988bc0af8a710ba5c5b4413d7558/services.json').json()
-services = c.catalog.service('wireguard')[1]
-for service in services:
-    service_env = service['NodeMeta']['env']
-    ip = service['ServiceAddress']
-    if service_env in rules:
-        rules[service_env]['ips'].append(ip)
+def get_current_host_env() -> str:
+    if DEBUG_CONSUL_CURRENT_HOST_ENV_OVERRIDE:
+        logging.info('Using DEBUG_CONSUL_CURRENT_HOST_ENV_OVERRIDE')
+        env = DEBUG_CONSUL_CURRENT_HOST_ENV_OVERRIDE
     else:
-        rules[service_env] = {'ips': []}
+        agent_info = c.agent.self()
+        env = agent_info['Config']['NodeMeta']['env']
+    logging.info(f'Current host env: {env}')
 
-add_service_to_zone_rules(rules, 'metrics', 'node_exporter')
+    return env
 
-if current_node_env == 'app':
-    add_service_to_zone_rules(rules, 'metrics', 'mysqld_exporter')
-    add_service_to_zone_rules(rules, 'backups', 'mysql')
 
-if current_node_env == 'logs':
-    add_service_to_zone_rules(rules, 'metrics', 'logstash_syslog')
-    add_service_to_zone_rules(rules, 'backups', 'logstash_syslog')
-    add_service_to_zone_rules(rules, 'app', 'logstash_syslog')
+def get_env_node_ips() -> dict:
+    env_node_ips = {}
+    if DEBUG_CONSUL_SERVICES_OVERRIDE_JSON_URL:
+        logging.info('Getting Consul Services from DEBUG_CONSUL_SERVICES_OVERRIDE_JSON_URL:')
+        logging.info(DEBUG_CONSUL_SERVICES_OVERRIDE_JSON_URL)
+        services = requests.get(DEBUG_CONSUL_SERVICES_OVERRIDE_JSON_URL).json()
+    else:
+        services = c.catalog.service('wireguard')[1]
 
-apply_firewall_rules(rules)
+    for service in services:
+        service_env = service['NodeMeta']['env']
+        ip = service['ServiceAddress']
+        if service_env in env_node_ips:
+            env_node_ips[service_env]['ips'].append(ip)
+        else:
+            env_node_ips[service_env] = {'ips': []}
+
+    return env_node_ips
+
+
+def add_services_to_rules(rules: dict, env: str) -> dict:
+    add_service_to_zone_rules(rules, 'metrics', 'node_exporter')
+
+    if env == 'app':
+        add_service_to_zone_rules(rules, 'metrics', 'mysqld_exporter')
+        add_service_to_zone_rules(rules, 'backups', 'mysql')
+
+    if env == 'logs':
+        add_service_to_zone_rules(rules, 'metrics', 'logstash_syslog')
+        add_service_to_zone_rules(rules, 'backups', 'logstash_syslog')
+        add_service_to_zone_rules(rules, 'app', 'logstash_syslog')
+
+    return rules
+
+
+if __name__ == '__main__':
+    current_node_env = get_current_host_env()
+    env_node_ips = get_env_node_ips()
+    firewall_rules = add_services_to_rules(env_node_ips, current_node_env)
+    apply_firewall_rules(firewall_rules)
